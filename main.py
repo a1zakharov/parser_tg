c09b6f0726e77a1c52f67804af40b0073fb966d0
Alex <i@zaharov-1.ru>
2024-06-03 03:30:43 +0500

    Основной код

---
+++ main.py
@@ -0,0 +1,25 @@
+import requests
+from bs4 import BeautifulSoup as bs
+from pprint import pprint
+
+file = open('read.txt', 'w', encoding="utf-8")
+
+link = input('link: ') #https://t.me/markettwits
+index = link[13:]
+link_channel = requests.get('https://t.me/s/' + index)
+html = bs(link_channel.text, 'html.parser')
+
+massages = html.find_all('div', class_='tgme_widget_message_text js-message_text')
+times = html.find_all('time', class_='time')
+pprint(massages)
+
+def tg_times():
+    for time in times:
+        x = str(time.text)
+        return x
+
+
+for massage in massages:
+    file.write(str(massage.text) + ';' + str(tg_times()) + '\n')
+
+file.close()
